cef5bcfcd7d7ce3c61a19bfc93622334be5ec5b3
Ericson Weah <ericson.weah@gmail.com>
2023-05-08 10:52:08 -0600

    add findIndex test

--- functions/findIndex/.test.js
+++ functions/findIndex/.test.js
@@ -1,20 +1,56 @@
 'use strict';
 
-const findIndex  = require('./index');
+const findIndex = require('./index');
 
 /*
 |------------------------------------------------------------------------------------
 | SUIT TEST
 |------------------------------------------------------------------------------------
 |
-| This test
+| The first test case asserts that the findIndex function correctly returns the index of the first element in the array that satisfies the provided testing function.
+
+| The second test case asserts that the findIndex function returns -1 if no element in the array satisfies the provided testing function.
+
+| The third test case asserts that the findIndex function throws a TypeError if the first argument is not an array.
 |
 */
 
 describe('findIndex function', () => {
 
-  it('It should at least do something', () => {
 
+  /*
+  |------------------------------------------------------------------------------------
+  | Should return the index of the first element in the array that satisfies the provided testing function
+  |------------------------------------------------------------------------------------
+  */
+  test('returns the index of the first element in the array that satisfies the provided testing function', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const fn = (el) => el > 2;
+
+    expect(findIndex(arr, fn)).toEqual(2);
+  });
+
+  /*
+  |------------------------------------------------------------------------------------
+  | Should return -1 if no element in the array satisfies the provided testing function
+  |------------------------------------------------------------------------------------
+  */
+  test('returns -1 if no element in the array satisfies the provided testing function', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const fn = (el) => el > 5;
+
+    expect(findIndex(arr, fn)).toEqual(-1);
+  });
+
+  /*
+  |------------------------------------------------------------------------------------
+  | Should throws a TypeError if the first argument is not an array
+  |------------------------------------------------------------------------------------
+  */
+  test('throws a TypeError if the first argument is not an array', () => {
+    const fn = () => findIndex('not an array', () => { });
+
+    expect(fn).toThrow(TypeError);
   });
 
 });
